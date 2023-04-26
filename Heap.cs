5f6dc71f336337444d9d3992e86b6b1a3755e6c3
hannyoo <119824566+hannyoo@users.noreply.github.com>
2023-04-26 18:30:03 +0900

    Data Study

---
+++ Heap.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace HeapData
+{
+    public class Heap<T>
+    {
+        
+        private List<int> data;
+
+        public Heap( )
+        {
+            data = new List<int>( );
+        }
+
+        public int RootNode( )
+        {
+            return data[0];
+        }
+
+        public int LastNode( )
+        {
+            return data[data.Count - 1];
+        }
+
+        public int LeftChildIndex(int index)
+        {
+            return (index * 2) + 1;
+        }
+
+        public int RightChildIndex(int index)
+        {
+            return (index * 2) + 2;
+        }
+
+        public int ParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        public void Insert(int value)
+        {
+            data.Add(value);
+            int newNodeIndex = data.Count - 1;
+
+            while(newNodeIndex > 0 && data[newNodeIndex] >
+                   data[ParentIndex(newNodeIndex)])
+            {
+                int parentIndex = ParentIndex(newNodeIndex);
+                int temp = data[parentIndex];
+                data[parentIndex] = data[newNodeIndex];
+                data[newNodeIndex] = temp;
+                newNodeIndex = parentIndex;
+            }
+        }
+
+        public void Delete( )
+        {
+            data[0] = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            int trickleNodeIndex = 0;
+
+            while(HasGreaterChild(trickleNodeIndex))
+            {
+                int largerChildIndex = CalculateLargerChildIndex(trickleNodeIndex);
+                int temp = data[trickleNodeIndex];
+                data[trickleNodeIndex] = data[largerChildIndex];
+                data[largerChildIndex] = temp;
+
+                trickleNodeIndex = largerChildIndex;
+            }
+        }
+
+        private bool HasGreaterChild(int index)
+        {
+            bool hasLeftChild = LeftChildIndex(index) < data.Count;
+            bool hasRightChild = RightChildIndex(index) < data.Count;
+
+            if(hasLeftChild && hasRightChild)
+            {
+                return data[LeftChildIndex(index)] > data[index] || data[RightChildIndex(index)] > data[index];
+            }
+            else if(hasLeftChild)
+            {
+                return data[LeftChildIndex(index)] > data[index];
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int CalculateLargerChildIndex(int index)
+        {
+            int leftChildIndex = LeftChildIndex(index);
+            int rightChildIndex = RightChildIndex(index);
+
+            if(rightChildIndex < data.Count && data[rightChildIndex] > data[leftChildIndex])
+            {
+                return rightChildIndex;
+            }
+            else
+            {
+                return leftChildIndex;
+            }
+        }
+
+    }
+}
---
+++ HeapTry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace HeapData
+{
+	public class HeapTry
+	{
+        static void Main(string[] args)
+        {
+
+            Heap<int> myHeap = new Heap<int>( );
+
+            myHeap.Insert(5);
+            myHeap.Insert(10);
+            myHeap.Insert(3);
+            myHeap.Insert(8);
+            myHeap.Insert(7);
+            myHeap.Insert(2);
+            myHeap.Insert(6);
+            myHeap.Insert(1);
+            myHeap.Insert(9);
+            myHeap.Insert(4);
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(myHeap.RootNode( ));
+                myHeap.Delete( );
+            }
+
+        }
+    }
+}
+
